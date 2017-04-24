a08b10fff8b0a96bcf86f972c2c3b40b9a07faad
ulubeyn <nadirulubey@gmail.com>
2017-04-24 17:39:20 +0300

    BaseService exported and comments added #6

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { WebRequest } from "./requests/web.request";
+export { BaseService } from "./services/base.service";
 export { BaseRequest } from "./requests/base.request";
 export { RequestHandler } from "./handlers/request.handler";
 export { ResponseHandler } from "./handlers/response.handler";
--- src/services/base.service.ts
+++ src/services/base.service.ts
@@ -33,45 +33,84 @@ export abstract class BaseService {
       */
      protected abstract handlerError<T>(response: Response): T;
 
+     /**
+      * Performs a request with `get` http method.
+      * @param url Url of the request
+      * @param options Options
+      */
      protected httpGet<T>(url: string, options?: RequestOptionsArgs): Observable<T> {
           return this._http.get(url, options)
                .map(response => this.extractData.call(this, response))
                .catch(error => this.handlerError.call(this, error));
      }
 
+     /**
+      * Performs a request with `post` http method.
+      * @param url Url of the request
+      * @param body Body
+      * @param options Options
+      */
      protected httpPost<T>(url: string, body: any, options?: RequestOptionsArgs): Observable<T> {
           return this._http.post(url, body, options)
                .map(response => this.extractData.call(this, response))
                .catch(error => this.handlerError.call(this, error));
      }
 
+     /**
+      * Performs a request with `put` http method.
+      * @param url Url of the request
+      * @param body Body
+      * @param options Options
+      */
      protected httpPut<T>(url: string, body: any, options?: RequestOptionsArgs): Observable<T>{
           return this._http.put(url, body, options)
                .map(response => this.extractData.call(this, response))
                .catch(error => this.handlerError.call(this, error));
      }
 
+     /**
+      * Performs a request with `delete` http method.
+      * @param url Url of the request
+      * @param options Options
+      */
      protected httpDelete<T>(url: string, options?: RequestOptionsArgs): Observable<T>{
           return this._http.delete(url, options)
                .map(response => this.extractData.call(this, response))
                .catch(error => this.handlerError.call(this, error));
      }
 
+     /**
+      * Performs a request with `patch` http method.
+      * @param url Url of the request
+      * @param body Body
+      * @param options Options
+      */
      protected httpPatch<T>(url: string, body: any, options?: RequestOptionsArgs): Observable<T>{
           return this._http.patch(url, body, options)
                .map(response => this.extractData.call(this, response))
                .catch(error => this.handlerError.call(this, error));
      }
 
+     /**
+      * Performs a request with `head` http method.
+      * @param url Url of the request
+      * @param options Options
+      */
      protected httpHead<T>(url: string, options?: RequestOptionsArgs): Observable<T>{
           return this._http.head(url, options)
                .map(response => this.extractData.call(this, response))
                .catch(error => this.handlerError.call(this, error));
      }
 
+     /**
+      * Performs a request with `options` http method.
+      * @param url Url of the request
+      * @param options Options
+      */
      protected httpOptions<T>(url: string, options?: RequestOptionsArgs): Observable<T>{
           return this._http.options(url, options)
                .map(response => this.extractData.call(this, response))
                .catch(error => this.handlerError.call(this, error));
      }
+     
 }
