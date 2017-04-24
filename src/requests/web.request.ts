43019b58374970bb0bcec9f6f11526839f8e434f
ulubeyn <nadirulubey@gmail.com>
2017-04-24 13:12:10 +0300

    BaseResponse removed

--- src/requests/web.request.ts
+++ src/requests/web.request.ts
@@ -1,14 +1,13 @@
 import { Observable } from "rxjs/Observable";
 
 import { BaseRequest } from "./base.request";
-import { WebResponse } from "../responses/web.response";
 import { RequestHandler } from "../handlers/request.handler";
 import { ResponseHandler } from "../handlers/response.handler";
 
 /**
  * Web request
  */
-export abstract class WebRequest<T extends WebResponse> extends BaseRequest<T>{
+export abstract class WebRequest<TResponse> extends BaseRequest<TResponse>{
 
      constructor(requestHandler: RequestHandler) {
           super(requestHandler);
@@ -17,13 +16,13 @@ export abstract class WebRequest<T extends WebResponse> extends BaseRequest<T>{
      /**
       * Gets HTTP call for the current request.
       */
-     protected abstract getHttpCall(): Observable<T>;
+     protected abstract getHttpCall(): Observable<TResponse>;
 
      /**
       * Executes current request with `getHttpCall()`. Invoked when `send` method called in {@link BaseRequest}.
       * @param responseHandler Response handler 
       */
-     protected onSend(responseHandler: ResponseHandler<T>): void {
+     protected onSend(responseHandler: ResponseHandler<TResponse>): void {
           this.getHttpCall().subscribe(
                response => this.onSuccessResponseReceived(response, responseHandler),
                error => this.onErrorResponseReceived(error, responseHandler)
